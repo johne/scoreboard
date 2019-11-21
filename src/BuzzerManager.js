720a74e4b47f2e7c17e3089d4a1fbf403d2fabf5
John Ellis <jellis@billfire.com>
2019-11-20 22:26:48 -0700

    stuff

--- src/BuzzerManager.js
+++ src/BuzzerManager.js
@@ -1,18 +1,25 @@
-const Gpio = require("onoff").Gpio;
+const Gpio = require("pigpio").Gpio;
 
 class BuzzerManager {
   construbtor() {
-    this.buzzer = new Gpio(17, "out");
+    this.buzzer1 = new Gpio(19, Gpio.OUTPUT);
+    this.buzzer2 = new Gpio(26, Gpio.OUTPUT);
+
+    this.timer = setTimeout(() => {
+      this._buzzIt();
+    }, 20000);
   }
 
   _buzzIt() {
     console.log("buzzing");
-    // this.timer = undefined;
-    // this.buzzer.writeSync(1);
-    // setTimeout(() => {
-    //   console.log("stopping");
-    //   this.buzzer.writeSync(0);
-    // }, 300);
+    this.timer = undefined;
+    this.buzzer1.hardwarePwmWrite(261626, 1000000);
+    this.buzzer2.hardwarePwmWrite(261, 1000000 / 2);
+    setTimeout(() => {
+      console.log("stopping");
+      this.buzzer1.hardwarePwmWrite(0, 0);
+      this.buzzer2.hardwarePwmWrite(0, 0);
+    }, 300);
   }
 
   processUpdate(update) {
