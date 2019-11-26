0fe5f4d99a842f7bb24ad7c3b5737414f6db0805
John Ellis <jellis@billfire.com>
2019-11-26 07:47:45 -0700

    stuff

---
+++ bin/shutdown.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python 
+# Simple script for shutting down the raspberry Pi at the press of a button.  
+# by Inderpreet Singh  
+  
+import RPi.GPIO as GPIO  
+import time  
+import os 
+import threading 
+ 
+# Use the Broadcom SOC Pin numbers  
+# Setup the Pin with Internal pullups enabled and PIN in reading mode.  
+GPIO.setmode(GPIO.BCM)  
+GPIO.setup(3, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)  
+
+t = 0
+
+def shutdown():
+	print("shutting down")
+	os.system("sudo shutdown -h now")
+
+def button_event(channel):
+	global t
+	if GPIO.input(3) == 1:
+		print("released")
+		t.cancel()
+	else:
+		print("pressed")
+		if t != 0:
+			t.cancel()
+		t = threading.Timer(3.0, shutdown)
+		t.start()
+
+# Add our function to execute when the button pressed event happens  
+GPIO.add_event_detect(3, GPIO.BOTH, callback = button_event, bouncetime = 50)  
+ 
+# Now wait!  
+while 1:  
+    time.sleep(0.1) 
