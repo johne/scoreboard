6e99d963c1547dd4d01969e84cbacf88f23a4e8e
John Ellis <jellis@billfire.com>
2020-01-07 22:22:00 -0700

    deploying

---
+++ ui/scoreboard/build/precache-manifest.61a784cce346ea3486c197497bcc33a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eabf74fd1c431354597f96e718889c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7447f10d647c331ec22",
+    "url": "/static/css/2.47fb9b64.chunk.css"
+  },
+  {
+    "revision": "ff5489bc029840aa82ed",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e7447f10d647c331ec22",
+    "url": "/static/js/2.9a7fbecf.chunk.js"
+  },
+  {
+    "revision": "5393f34f9b854dc2af4199722e0759f4",
+    "url": "/static/js/2.9a7fbecf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff5489bc029840aa82ed",
+    "url": "/static/js/main.19f008db.chunk.js"
+  },
+  {
+    "revision": "a2d9ecdfec5342800abc",
+    "url": "/static/js/runtime-main.7653e7cc.js"
+  }
+]);
